1897b5550c89b868de5da113a5580e19c3e771fa
Gijsjan <agijsbro@gmail.com>
2014-07-08 12:02:05 +0200

    Initail

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+(function() {
+  module.exports = {
+    str: function(str) {
+      return {
+
+        /* Escape a regular expression */
+        escapeRegExp: function() {
+          return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+        },
+        ucfirst: function() {
+          return str.charAt(0).toUpperCase() + str.slice(1);
+        },
+        slugify: function() {
+          var from, index, strlen, to;
+          from = "àáäâèéëêìíïîòóöôùúüûñç·/_:;";
+          to = "aaaaeeeeiiiioooouuuunc-----";
+          str = str.trim().toLowerCase();
+          strlen = str.length;
+          while (strlen--) {
+            index = from.indexOf(str[strlen]);
+            if (index !== -1) {
+              str = str.substr(0, strlen) + to[index] + str.substr(strlen + 1);
+            }
+          }
+          return str.replace(/[^a-z0-9 -]/g, '').replace(/\s+|\-+/g, '-').replace(/^\-+|\-+$/g, '');
+        }
+      };
+    }
+  };
+
+}).call(this);
---
+++ gulpfile.coffee
@@ -0,0 +1,14 @@
+gulp = require 'gulp'
+uglify = require 'gulp-uglify'
+streamify = require 'gulp-streamify'
+rename = require 'gulp-rename'
+coffee = require 'gulp-coffee'
+
+gulp.task 'build', ->
+  gulp.src('./src/**/*')
+    .pipe(coffee())
+    .pipe(rename(basename: 'index'))
+    .pipe(gulp.dest("./dist"))
+    .pipe(streamify(uglify()))
+    .pipe(rename(extname: '.min.js'))
+    .pipe(gulp.dest("./dist"))
